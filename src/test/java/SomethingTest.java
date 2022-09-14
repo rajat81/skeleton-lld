684bb1da25935cec590454f250d2a2fc1f760493
rajatsawant <rajat.sawant@zeptonow.com>
2022-09-14 14:53:12 +0530

    create skeleton

---
+++ src/test/java/SomethingTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class SomethingTest {
+    @Before
+    public void doThisBeforeAnything() {
+
+    }
+
+    @Test
+    public void getThisTested() {
+
+    }
+}
